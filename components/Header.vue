e10dca03b0f9e1da3ba35b4fbb29b02838928260
Mark de Neut Brossois <mdeneutbrossois@bol.com>
2021-10-09 19:58:44 +0200

    First setup of project scss and default file

---
+++ components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="header">
+    <p>Home</p>
+    <p>Previous lessons</p>
+    <p>Overview</p>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Header />
+    <nuxt />
+    <p>Footer</p>
+  </div>
+</template>
+<script>
+import Header from "../components/Header";
+export default {
+  components: {Header}
+}
+</script>
