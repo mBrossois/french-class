632d51d9cbab9c364845fe67969c29ea985719db
Mark de Neut Brossois <mdeneutbrossois@bol.com>
2021-10-10 18:14:49 +0200

    Added top nav

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,58 @@
 <template>
   <div class="header">
-    <p>Home</p>
-    <p>Previous lessons</p>
-    <p>Overview</p>
+    <ui-tab-bar v-model="active" type="textWithIcon" class="custom-demo-tab-bar">
+      <ui-tab
+        v-for="(tab, index) in tabs"
+        :key="index"
+        type="textWithIcon"
+        :icon="tab.icon"
+        v-on:click="updateRoute(tab.url, $router)"
+      >
+        {{ tab.text }}
+
+      </ui-tab>
+    </ui-tab-bar>
   </div>
 </template>
+
+<script lang="ts">
+import VueRouter from 'vue-router'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $router: VueRouter
+  }
+}
+
+const tabs = [
+  {
+    icon: 'class',
+    url: '/today-lesson',
+    text: "Today's lesson"
+  },
+  {
+    icon: 'menu_book',
+    url: '/previous-lessons',
+    text: "Previous lessons"
+  },
+  {
+    icon: 'insights',
+    url: '/overview',
+    text: "Overview"
+  }
+];
+
+export default  {
+  data() {
+    return {
+      tabs,
+      active: 0
+    };
+  },
+  methods: {
+    updateRoute(url: string, router: VueRouter) {
+      router.push(url);
+    }
+  }
+};
+</script>
