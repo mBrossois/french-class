6cce42ae292ac632942275215fb2e075390a7090
Mark de Neut Brossois <mdeneutbrossois@bol.com>
2021-10-13 23:09:42 +0200

    Progressbar get props and display blue for active one

--- components/Header.vue
+++ components/Header.vue
@@ -42,7 +42,7 @@ export default  {
     };
   },
   methods: {
-    updateRoute(url: string, router: VueRouter) {
+    updateRoute(url: string, router: any) {
       router.push(url);
     }
   }
--- components/ProgressBar.vue
+++ components/ProgressBar.vue
@@ -1,38 +1,80 @@
 <template>
-  <div class="progress-bar">
-    <div class="arrow" v-for="(step, index) in progressBar" :key="index">
-        {{ step.value }}
+  <div class="container">
+    <div class="progress-bar">
+      <div class="step" v-for="(step, index) in progressBar" :key="index" :class="index === 0 ? 'first' : ''">
+        <div class="content" :class="active === index ? 'active' : ''">{{ step.value }}</div>
+        <div v-if="index !== (progressBar.length - 1)" :class="active > index" class="line"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
-.progress-bar {
+.container {
   width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.progress-bar {
+  width: 80%;
   height: 4em;
-  background: #81d4fa;
   display: flex;
   justify-content: space-around;
   align-items: center;
+
+  .step.first {
+    margin-left: 10%;
+  }
+
+  .step {
+    display: flex;
+    align-items: center;
+    flex-grow: 1;
+
+    .line {
+      height: 2px;
+      background: grey;
+      flex-grow: 1;
+    }
+
+    .line.active {
+      background: #81d4fa;
+    }
+
+    .content {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 1em;
+      border: 1px solid grey;
+      border-radius: 10px;
+    }
+
+    .content.active {
+      border: 1px solid #81d4fa;
+    }
+  }
+
 }
 
-.arrow {
-  width: 33%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.arrow.active {
+  background: #81d4fa;
 }
 </style>
 
 <script lang="ts">
-
 export default {
-  props: {'initialProgressBar': {type: Object, default: null}},
-  data() {
+  name: "ProgressBar",
+  active: 0,
+  props: {initialProgressBar: {type: Array, default: Array}},
+  data(): any {
     return {
-      progressBar: this.initialProgressBar
+      progressBar: this.initialProgressBar,
+      active: 0
     }
+  },
+  methods: {
   }
 }
 </script>
